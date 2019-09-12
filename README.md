edff1cd38f5449e7b6a846aa280b61654709b6d0
Josh Ashley <jashley2017@gmail.com>
2019-09-11 22:51:18 -0400

    Setup initial app for rover

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LunaRover
 Code that goes on the rover for the NASA 2020 Lunabotics Competition
+
+To setup and run do: 
+```shell
+git clone https://github.com/UKyKORA/LunaRover.git
+cd LunaRover
+python bin/launch_rover.py
+```
---
+++ bin/launch_rover.py
@@ -0,0 +1,12 @@
+'''
+Title: Rover Launch App
+Author: Josh Ashley
+Description:
+    Central application for starting up the rover box's core functions.
+'''
+class LunaRover():
+    def run():
+        print("Rover Startup")
+
+if __name__ == "__main__":
+    LunaRover.run()
