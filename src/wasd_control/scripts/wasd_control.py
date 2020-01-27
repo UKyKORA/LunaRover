65173f06f5129856e226e5ba3fae94d797d79b2a
jashley2017 <jashley2017@gmail.com>
2020-01-27 14:48:07 -0500

    adding websocket functionality in submodules

--- src/wasd_control/scripts/wasd_control.py
+++ src/wasd_control/scripts/wasd_control.py
@@ -1,3 +1,8 @@
+#! /usr/bin/env python3
+'''
+Simple WASD Control on the host robot for testing.
+'''
+
 import rospy
 from rover_msgs.msg import FWDSetting
 
@@ -6,18 +11,20 @@ class WASDControl():
     def __init__(self, pub):
         self.pub = pub
         self.directions = {
-            "w": (50, 50, 50, 50)
+            "w": (50, 50, 50, 50),
             "a": (50, 50, -50, -50),
             "s": (-50, -50, -50, -50),
             "d": (-50, -50, 50, 50)
         }
 
     def spin(self):
-        while(True):
+        '''a loop to recieve inputs for wasd controls locally on the bot'''
+        while True:
             direction = input("")
             self.set_speeds(*self.directions.get(direction, None))
 
-    def set_speeds(sp1, sp2, sp3, sp4):
+    def set_speeds(self, sp1, sp2, sp3, sp4):
+        '''set the speeds in the FWDSetting Message'''
         new_setting = FWDSetting()
         new_setting.front_left = sp1
         new_setting.back_left = sp2
@@ -26,8 +33,12 @@ class WASDControl():
         self.pub.publish(new_setting)
 
 def launch():
+    '''ROS node launcher'''
     rospy.init_node('wasd_control')
     drive_pub = rospy.Publisher('drive_setting', FWDSetting, queue_size=10)
 
     wasd = WASDControl(drive_pub)
     wasd.spin()
+
+if __name__ == '__main__':
+    launch()
