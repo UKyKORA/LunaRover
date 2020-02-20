8e6ec6577c94f18f6b479d468850b0b283cbaeda
jashley2017 <jashley2017@gmail.com>
2020-02-20 15:22:26 -0500

    merged multimode

--- src/drive/scripts/drive_serial.py
+++ src/drive/scripts/drive_serial.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 '''
 
-ROS Node for 4WD controls
+ROS Node for 4WD controls from a web client
 
 '''
 import rospy
@@ -20,22 +20,27 @@ class SerialMotorControl:
         self.serial_controller.set_vel(msg.motor_id, msg.motor_value)
 
     def assign_motor_id(self, msg):
-        if msg.connect_or_disconnect and self.available_motors != []:
-
-            # track client
-            self.client_motors[msg.client_key] = self.available_motors.pop()
-
-            # send ROS message out to client
+        if msg.connect_or_disconnect and len(self.available_motors) >= msg.motor_count:
+            self.client_motors[msg.client_key] = []
             motor_assign_msg = MotorId()
-            motor_assign_msg.assigned_id = self.client_motors[msg.client_key]
+            temp_assignment = []
+            # send ROS message out to client
+            for _bleh in range(msg.motor_count):
+                new_motor = self.available_motors.pop()
+                self.client_motors[msg.client_key].append(new_motor)
+                temp_assignment.append(new_motor)
+            motor_assign_msg.assigned_id = tuple(temp_assignment)
             motor_assign_msg.client_key = msg.client_key
             self.client_pub.publish(motor_assign_msg)
 
         elif not msg.connect_or_disconnect and msg.client_key in self.client_motors:
 
             # delete client and free up the motor again
-            self.available_motors.append(self.client_motors[msg.client_key])
+            for mtr in self.client_motors[msg.client_key]:
+                self.available_motors.append(mtr)
             self.client_motors.pop(msg.client_key)
+        elif msg.connect_or_disconnect:
+            rospy.loginfo("not enough motors to assign, skipping")
 
 
 def launch():
